c38e29f7ed972ec18f7feac136cb1628566e27fd
Ramin Mammadzada <raminmammadzadaiu@gmail.com>
2022-09-01 20:15:02 +0300

    Add logger.

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +24,8 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -48,9 +51,11 @@ public class UserController {
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
 
+		log.info("User name set wıth ", createUserRequest.getUsername());
+
 		if(createUserRequest.getPassword().length() < 7 ||
 			!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
-//			log.error("Error with user password. Cannot create user {}", createUserRequest.getUsername());
+			log.error("Error with user password. Cannot create user {}", createUserRequest.getUsername());
 			return ResponseEntity.badRequest().build();
 		}
 
